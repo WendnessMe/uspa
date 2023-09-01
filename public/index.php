5acf52c1d43f78d7f5aca1af2efe5f52e4dd1696
Keven Wilson <k.even10@hotmail.com>
2023-09-01 16:02:19 -0300

    Remove unnecessary code

--- public/index.php
+++ public/index.php
@@ -24,29 +24,17 @@ $routes = [
   '/api' => 'ApiController@hi',
 ];
 
-// $test = new WendnessMe\Uspa\Controllers\ApiController;
-// echo "Test import: ";
-// echo $test->hi() . "<br>";
-// $cont = "WendnessMe\Uspa\Controllers\ApiController";
-// $import = new $cont();
-
-echo "Dynamic import: ";
-// echo $import->hi() . "<br>";
-
 if (array_key_exists($uri, $routes)) {
   $controllerArray = explode("@", $routes[$uri]);
   $controller = $controllerArray[0];
   $cont = "WendnessMe\Uspa\Controllers\\" . $controller;
   $import = new $cont();
-  echo "Import from inside: ";
   echo $import->hi();
-  // $controller = new \WendnessMe\Uspa\Controllers;
-  // print_r($routes[$uri]);
-  // print_r($controllerArray);
   echo "<br>";
 }
 
 if (!empty($routes)) {
+  // To-do:
   // Create function to check route independently of the route defined in the array
   // whether it was defined with a / (slash) or not.
   if (array_key_exists($uri, $routes) == false) {
@@ -58,18 +46,3 @@ if (!empty($routes)) {
 
 echo "URI: " . $uri . "<br>";
 $controllerArray = explode("@", $routes['/']);
-
-// $utils->dd($controllerArray);
-
-
-// $count = 1;
-// foreach ($routes as $route) {
-//   $contArr = explode("@", $route);
-//   echo "<h1>Controller $count:</h1>";
-//   echo "<b>Class:</b> $contArr[0] <br>";
-//   echo "<b>Method:</b> $contArr[1] <br>";
-//   $count += 1;
-//   // echo "<pre>";
-//   // print_r(explode("@", $route));
-//   // echo "</pre>";
-// }
