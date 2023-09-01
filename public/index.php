d4bfec48f3e75aea8767227fbc0bf38563b8f8c7
Keven Wilson <k.even10@hotmail.com>
2023-09-01 12:50:50 -0300

    Basic flow control for index file

--- public/index.php
+++ public/index.php
@@ -5,44 +5,39 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use WendnessMe\Uspa\Config;
 use WendnessMe\Uspa\Utils;
 
+$utils = new Utils();
 $config = new Config();
-
 $res = $config->query("SELECT * FROM test");
-var_dump($res);
-class Data
+// var_dump($res);
+
+$uri = $_SERVER['REQUEST_URI'];
+
+$routes = [
+  '/' => 'HomeController@index',
+  '/test' => 'HomeController@test',
+  'outro' => 'HomeController@outro',
+];
+
+function routeCheck ($route, $array)
 {
-  public $pdo;
-
-  public function __construct ()
-  {
-    $this->pdo = new PDO("sqlite:" . __DIR__ . "/../database/test.db");
-  }
-
-  public function query ($query)
-  {
-    // var_dump($this->pdo);
-
-    $utils = new Utils();
-    // $utils->dd(__DIR__ . '/test.db');
-    $state = $this->pdo->prepare($query);
-    $state->execute();
-
-    return $state->fetchAll(PDO::FETCH_ASSOC);
+  if (array_key_exists($route, $array)) {
+    return true;
+  } else {
+    return false;
   }
 }
 
-$class = new Data();
-$res = $class->query("SELECT * FROM test");
+if (!empty($routes)) {
+  // echo $_SERVER['REQUEST_URI'];
+  // echo routeCheck($uri, $routes);
+  if (routeCheck($uri, $routes) == false) {
+    echo "404";
+    exit;
+  }
 
-// var_dump($res);
+  // print_r(array_key_exists('/', $routes));
+  // if (array_key_exists('outro', $array)) {
+  //   echo "FOda";
+  // }
+}
 
-// $pdo = new PDO("sqlite:test.db");
-// $statement = $pdo->prepare("SELECT * FROM test");
-// $statement->execute();
-// $res = $statement->fetchAll(PDO::FETCH_ASSOC);
-// var_dump($res);
-
-// $config->pdo->prepare("SELECT * FROM test");
-// $config->pdo->execute();
-// var_dump($config->pdo);
-// echo $config->hi();
